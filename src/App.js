4a017865518df3ecad3fd065f199ee4746cc695d
studmanj <bjackso3@hotmail.com>
2022-09-07 23:40:18 -0600

    Hero Container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 import "./css/styles.css"
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
         <Navbar />
+        <Hero />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,10 @@
 function Navbar() {
     return (
-        <>
         <nav className="navbar">
             <img src="images/airbnb-logo.png" 
                  className="navbar-logo"  
                  alt="Airbnb logo" />
-        </nav>
-            <img src="images/photo-grid.png" 
-                 className="navbar-grid"  
-                 alt="Airbnb grid" />
-        
-        </>
-            
+        </nav>  
     )
 }
 
