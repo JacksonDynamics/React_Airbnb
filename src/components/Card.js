bbc4cf1a01739df4bc1df4b07cdff0f0d986f173
studmanj <bjackso3@hotmail.com>
2022-09-13 00:35:18 -0600

    Simplify props into one component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,30 @@
 function Card(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
-    }
+    }     
+
     return( 
         <div className="card">
             {badgeText && 
             <div className="card--badge">{badgeText}</div>}
-            <img src={`./images/${props.img}`}
+            <img src={`./images/${props.item.coverImg}`}
                  className="card--image" 
                  alt="" />
             <p className="rating">
                 <img src={"images/star.png"} 
                  className="star"
                  alt="Red rating star" /> 
-                 <span>{props.rating}</span>
-                 <span className="grey">({props.reviewCount}) • </span>
-                 <span className="grey">{props.location}</span> 
+                 <span>{props.item.stats.rating}</span>
+                 <span className="grey">({props.item.stats.reviewCount}) • </span>
+                 <span className="grey">{props.item.location}</span> 
             </p>
             <p className="card--title">
-                {props.title}
+                {props.item.title}
             </p>
-            <p className="card--cost"><strong>From ${props.price}</strong>/person</p>
+            <p className="card--cost"><strong>From ${props.item.price}</strong>/person</p>
         </div>
     )
 }
